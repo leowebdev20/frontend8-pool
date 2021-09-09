6f9cfd9927a79b4b6a44f4768d4861aeab004473
Leonardo Seren <leonardo.seren@primoround.com>
2021-09-09 12:51:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Auth } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
 function App() {
+  Auth.currentAuthenticatedUser({
+    bypassCache: false, // Optional, By default is false. If set to true, this call will send a request to Cognito to get the latest user data
+  })
+    .then((user) => console.log("user", user))
+    .catch((err) => console.log("error", err));
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,11 +23,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!
         </a>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App)
